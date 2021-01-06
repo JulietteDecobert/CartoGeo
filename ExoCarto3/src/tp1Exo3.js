581c13557e7e220345bcf66991df65b64ff2dec2
JulietteDecobert <54988613+JulietteDecobert@users.noreply.github.com>
2021-01-06 11:46:58 +0100

    add code

--- ExoCarto3/src/tp1Exo3.js
+++ ExoCarto3/src/tp1Exo3.js
@@ -1,16 +1,18 @@
-window.addEventListener('touchstart', touchEventHandler) ;
-  window.addEventListener('touchend', touchEventHandler) ;
-  window.addEventListener('touchcancel', touchEventHandler) ;
-  window.addEventListener('touchleave', touchEventHandler) ;
-  window.addEventListener('touchmove', touchEventHandler) ;
+$(document).ready(function() {
+  window.addEventListener('touchstart', touchEventHandler) ;
+    window.addEventListener('touchend', touchEventHandler) ;
+    window.addEventListener('touchcancel', touchEventHandler) ;
+    window.addEventListener('touchleave', touchEventHandler) ;
+    window.addEventListener('touchmove', touchEventHandler) ;
 
-}) ;
+  }) ;
 
-function touchEventHandler(event) {
-  var firstDiv = exercice3.find('div') ;
-  firstDiv.find('ul').empty() ;
-  firstDiv.find('ul').append(
-    "<li>Position X : " + event.changedTouches[0].screenX + "</li>",
-    "<li>Position Y : " + event.changedTouches[0].screenY + "</li>",
-    "<li>Type event : " + event.type + "</li>"
-  ) ;
+  function touchEventHandler(event) {
+    var firstDiv = exercice3.find('div') ;
+    firstDiv.find('ul').empty() ;
+    firstDiv.find('ul').append(
+      "<li>Position X : " + event.changedTouches[0].screenX + "</li>",
+      "<li>Position Y : " + event.changedTouches[0].screenY + "</li>",
+      "<li>Type event : " + event.type + "</li>"
+    ) ;
+}
