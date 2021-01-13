1911b3473392ca2180b15cd57d0412076ead822f
JulietteDecobert <54988613+JulietteDecobert@users.noreply.github.com>
2021-01-13 10:23:48 +0100

    tp2

--- TP2/Exo2/src/tp2Exo2.js
+++ TP2/Exo2/src/tp2Exo2.js
@@ -6,13 +6,21 @@ function initMap() {
   // On initialise la carte
   var carte = L.map('maCarte').setView([29.1222349,-80.991361], 1);
 
-  // On charge les "tuiles"
-  L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
-      // Il est toujours bien de laisser le lien vers la source des données
-      attribution: 'données © <a href="//osm.org/copyright">OpenStreetMap</a>/ODbL - rendu <a href="//openstreetmap.fr">OSM France</a>',
-      minZoom: 1,
-      maxZoom: 20
-  }).addTo(carte);
+  // L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
+  //     // Il est toujours bien de laisser le lien vers la source des données
+  //     attribution: 'données © <a href="//osm.org/copyright">OpenStreetMap</a>/ODbL - rendu <a href="//openstreetmap.fr">OSM France</a>',
+  //     minZoom: 1,
+  //     maxZoom: 20
+  // }).addTo(carte);
+
+  L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', {
+        attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+        subdomains: 'abcd',
+        minZoom: 0,
+        maxZoom: 20,
+        ext: 'png'
+  }).addTo(zeMap);
+
 
   let triangle = L.polygon([
       [29.1222349, -80.991361],
