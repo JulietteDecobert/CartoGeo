fb4237f5819b5b84db79ae48b3b2857f2130adbf
JulietteDecobert <54988613+JulietteDecobert@users.noreply.github.com>
2021-01-13 09:41:27 +0100

    tp2

--- TP2/Exo2/src/tp2Exo2.js
+++ TP2/Exo2/src/tp2Exo2.js
@@ -1,13 +1,10 @@
 $(document).ready(function() {
+initMap();
+
+}) ;
 
 // This example creates a simple polygon representing the Bermuda Triangle.
 function initMap() {
-  const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 5,
-    center: { lat: 24.886, lng: -70.268 },
-    mapTypeId: "terrain",
-  });
-  // Define the LatLng coordinates for the polygon's path.
   const triangleCoords = [
     { lat: 25.774, lng: -80.19 },
     { lat: 18.466, lng: -66.118 },
@@ -22,7 +19,11 @@ function initMap() {
     strokeWeight: 2,
     fillColor: "#FF0000",
     fillOpacity: 0.35,
+    });
+    bermudaTriangle.setMap(map);
+  }
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 5,
+    center: { lat: 24.886, lng: -70.268 },
+    mapTypeId: "terrain",
   });
-  bermudaTriangle.setMap(map);
-}
-}) ;
